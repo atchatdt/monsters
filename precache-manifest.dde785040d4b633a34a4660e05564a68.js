bb09f9146c4d6bc46ad12974ac63c6c13cb8adac
kuro <kisuvitinh@gmail.com>
2020-10-12 20:59:54 +0700

    Updates

---
+++ precache-manifest.dde785040d4b633a34a4660e05564a68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71c96ebde8cda50f705d5203158e50ad",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "e8320e43558fa6cb2629",
+    "url": "/monsters/static/css/main.f69c1596.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monsters/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8320e43558fa6cb2629",
+    "url": "/monsters/static/js/main.1282bc5f.chunk.js"
+  },
+  {
+    "revision": "3dab8aa598718cb8e8b3",
+    "url": "/monsters/static/js/runtime-main.f48c3991.js"
+  }
+]);
